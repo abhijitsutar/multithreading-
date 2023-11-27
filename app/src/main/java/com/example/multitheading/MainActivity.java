016647c8016369ff472d853729845ce6841de852
abhijitsutar <abhijitsutar9458@gmail.com>
2023-11-27 16:23:46 +0530

    MultiThreading

---
+++ app/src/main/java/com/example/multitheading/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.multitheading;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnDownload;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnDownload = findViewById(R.id.btnDownload);
+
+        btnDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //code to start download thread
+                String [] files = {
+                        "http://bitcode.in/android/demos.zip",
+                        "http://bitcode.in/java/demos.zip",
+                };
+                new DownloadThread(
+                        MainActivity.this, new MyHandler()).execute(files);
+
+            }
+        });
+    }
+    private class MyHandler extends Handler {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            if(msg.what == 2) {
+                btnDownload.setText("res = " + (float) msg.obj);
+            }
+            else {
+                btnDownload.setText((int)msg.obj + " % ");
+            }
+        }
+    }
+    private void mt(String text) {
+        Toast Toast = null;
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+
+
+}
+
